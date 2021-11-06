3617b0187b3dfece9db111261977bd2a8d876056
charlie <sirzapataj@gmail.com>
2021-11-05 22:31:10 -0500

    inicio migrar outlets a vue

--- resources/views/outlets/map.blade.php
+++ resources/views/outlets/map.blade.php
@@ -40,11 +40,9 @@
 
 
 <script type="text/javascript">
-    var 
-
-
-    map = L.map('mapid').setView([{{ config('leaflet.map_center_latitude') }}, {{ config('leaflet.map_center_longitude') }}], {{ config('leaflet.zoom_level') }});
-
+    //inicio script mapa lo q esta en config viene de un arreglo
+    var map = L.map('mapid').setView([{{ config('leaflet.map_center_latitude') }}, {{ config('leaflet.map_center_longitude') }}], {{ config('leaflet.zoom_level') }});
+    //inicio capa con el modelo de mapa, usando openstreetmap y mapboxstreets
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a> Powered by Jzapata',
@@ -52,22 +50,27 @@
     tileSize: 512,
     zoomOffset: -1
 }).addTo(map);
+//importante todo lo q quiera ser pintado en el mapa debe agregarse al nombre q tenga el mapa (map)
 
-
+//arreglo con las coordenadas del àrea aproximada del downtown miami
 var latlngs = [[25.756644, -80.190697],[25.761722, -80.199543],[25.766171, -80.199972],[25.766789, -80.200063],[25.766936, -80.200146],[25.768373, -80.199985],[25.771474, -80.198612],[25.772324, -80.198548],[25.777585, -80.203577],[25.778435, -80.206608],[25.778879, -80.195455],[25.780589, -80.195573],[25.781198, -80.195820],[25.786622, -80.196099],[25.788274, -80.196206],[25.794099, -80.194876],[25.791723, -80.194801],[25.791829, -80.185381],[25.787289, -80.185456],[25.777449, -80.185023],[25.779130, -80.183650],[25.778748, -80.183266],[25.775350, -80.185044],[25.774249, -80.184990],[25.769520, -80.182914],[25.769249, -80.182705],[25.769085, -80.182705],[25.763945, -80.184363],[25.756644, -80.190697]];
-
+// inicio funcion L.polygon proveniente de la librería leaflet
 var polygon = L.polygon(latlngs, {color: 'grey',title: "Coordinates"}).addTo(map);
 
-// zoom the map to the polygon
+// zoom the map to the polygon y su texto en a direcciòn centro y permanente true
 map.fitBounds(polygon.getBounds());
  polygon.bindTooltip("Downtown Miami",
    {permanent: true, direction:"center"}
   ).openTooltip()
 
+//coordenadas de arreglo de prueba
 var lati = 25.76272990;
 var lato = -80.19527350;
 var latin = 25.76246575;
 var laton = -80.19133949;
+
+
+// manera de llamar el control de ruta con dirección pintandola con una línea roja
 
 // var router = new L.Routing.OSRMv1();
 // for (var i = 0; i < routeWaypoints.length; i++) {
@@ -94,6 +97,8 @@ var laton = -80.19133949;
 // ];
 
 
+// manera de pintar marcador con ícono default en el mapa, 
+//a partir de un arreglo outlets.index y su response.data
 
     // var markers = L.markerClusterGroup();
 
@@ -115,6 +120,10 @@ var laton = -80.19133949;
 
 
 
+        // manera de llamar el control de ruta con dirección pintandola con una línea roja
+
+
+
 //   var routeWaypoints = [
 //   [new L.Routing.Waypoint([25.76272990, -80.19527350]), new L.Routing.Waypoint([25.76246575, -80.19133949])],
 // ];
@@ -127,6 +136,11 @@ var laton = -80.19133949;
 //         }
 //     });
 // }
+
+
+
+
+    // manera definitiva de llamar el control de ruta con dirección pintandola con una línea roja
 
 
     axios.get('{{ route('api.outlets.index') }}')
@@ -144,14 +158,16 @@ var control = L.Routing.control({
     }),
     geocoder: L.Control.Geocoder.nominatim({}),
     units: 'imperial',
-    addWaypoints: false // prevent users from adding new waypoints
+    addWaypoints: false // previene q los usuarios puedan agregar nueva dirección,
+                        // asumiendo q esta pantalla es la q pinta el pedido ya escogido
   
     }).addTo(map);
 
 
 
-
-function refreshDistanceAndLength() {
+// función para calcular la distancia y entregarla en millas a partir de un arreglo y 
+//trayéndose las dos primeras coordenadas en el index, la idea es q llamar a la matriz
+ function refreshDistanceAndLength() {
   distance = L.GeometryUtil.distance(map, response.data.features[0].properties.coordinate, response.data.features[1].properties.coordinate);
   length = L.GeometryUtil.length([response.data.features[0].properties.coordinate, response.data.features[1].properties.coordinate]);
   document.getElementById('distance').innerHTML = distance;
@@ -166,6 +182,7 @@ function refreshDistanceAndLength() {
         
 
 
+// manera de llamar el control de ruta con dirección pintandola con una línea roja
 
 
 // $.ajax({
@@ -192,6 +209,11 @@ function refreshDistanceAndLength() {
       
 
 
+
+// manera de llamar el control de ruta con dirección pintandola con una línea roja
+
+
+
 // var routeWaypoints = [
 //   [new L.Routing.Waypoint([25.76272990, -80.19527350]), new L.Routing.Waypoint([25.76246575, -80.19133949])],
 // ];
@@ -205,6 +227,9 @@ function refreshDistanceAndLength() {
 //     });
 // }
 
+
+
+//inicio función que agrega nueva coordenada a partir de un click en el mapa
 
     @can('create', new App\Outlet)
     var theMarker;
